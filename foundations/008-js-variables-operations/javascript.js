187f3e778c605eff2eba71dcc9ff88804c206976
brett-me <its-me-brett@protonmail.com>
2025-07-10 14:32:12 +0200

    add: variable, operations and console exercises

--- foundations/008-js-variables-operations/javascript.js
+++ foundations/008-js-variables-operations/javascript.js
@@ -1,1 +1,36 @@
-console.log("Hello, Computer");
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 10;
+console.log(age);
+
+age = 50;
+console.log(age);
+
+const pi = 3.14;
+console.log(pi);
+
+console.log(3 + 2 - 76 * (1 + 1));
+
+console.log(23 + 97);
+
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+a = 20;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
