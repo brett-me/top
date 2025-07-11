9fe02d655ac22c1fea37db0d4d6dc3a96f6d1bdc
brett-me <its-me-brett@protonmail.com>
2025-07-11 08:42:57 +0200

    js variables and operations exercises

--- foundations/008-js-variables-operations/javascript.js
+++ foundations/008-js-variables-operations/javascript.js
@@ -34,3 +34,36 @@ const max = 57;
 const actual = max - 13;
 const percentage = actual / max;
 console.log(percentage);
+
+let x = 5;
+x++; // increment (and decrement) by 1 ... increment only works on existing variables
+let z = x;
+console.log(z);
+
+let randomNumber = 10;
+randomNumber++;
+++randomNumber; // this also works (generally prioritise this, see below why)
+
+let wrongCounterNumber = randomNumber++; // this does not increment the variable
+console.log(wrongCounterNumber); // 12 rather than 13
+let correctCounterNumber = ++randomNumber;
+console.log(correctCounterNumber); // 14 as expected
+
+let number = 20;
+number--; // decrement by 1
+let newNumber = number;
+console.log(newNumber);
+
+let square = 5 ** 2; // operator
+let squareFunction = Math.pow(5, 2); // function
+console.log(square);
+console.log(squareFunction);
+
+let tenNumber = 10;
+let tenString = "10";
+
+console.log(tenNumber == tenString); // == equates values
+console.log(tenNumber === tenString); // === equates values and their datatypes
+
+console.log(tenString);
+console.log(+tenString); // + converts a variable to a number datatype
